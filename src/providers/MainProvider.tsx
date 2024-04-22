eca1f1dc6f1602f54df597ea7aa967126af570ba
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-23 00:06:14 +0500

    supabase, tanstack

---
+++ src/providers/MainProvider.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { PropsWithChildren } from "react"
+
+const queryClient = new QueryClient()
+
+export default function MainProvider({children, ...props}: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools/>
+    </QueryClientProvider>
+  )
+}
