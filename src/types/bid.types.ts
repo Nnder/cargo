35c0e48136f5071e5ba792c305d1fc3159e581e5
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-23 18:46:03 +0500

    update types

--- src/types/bid.types.ts
+++ src/types/bid.types.ts
@@ -1,12 +1,12 @@
 export type BidStatus = "новая" | "в работе" | "завершено";
 
 export interface Bid {
-    id: number | string
+    id: number
     created_at: number
     firm_name: string
     fio_carrier: string
     phone_carrier: string
     comments: string
     status: BidStatus
-    ati: number | string
+    ati: number
 }
