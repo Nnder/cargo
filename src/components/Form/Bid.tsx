dc41fee68c054459d30b0f935285ede5baa8d810
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-25 18:25:33 +0500

    label for form field

--- src/components/Form/Bid.tsx
+++ src/components/Form/Bid.tsx
@@ -82,42 +82,50 @@ export default function BidForm() {
     <Modal open={modal} onClose={() => closeModal()}>
       <div className={styles.form}>
           <FormProvider {...methods}>
-            <div style={{display: 'flex', flexDirection:'column', justifyContent: 'space-evenly', alignItems:'center', height: '450px',}}>
+            <div style={{display: 'flex', flexDirection:'column', justifyContent: 'space-evenly', alignItems:'flex-start', height: '450px',}}>
+              <div style={{color: '#fff'}}>№ заявки</div>
               <Controller name='id' control={control}
                 render={ ({field: {value, onChange}}) => (
                   <input className={styles.input} placeholder='№ заявки' value={value} onChange={onChange} disabled={true}/>
               )}/>
 
+              <div style={{color: '#fff'}}>Статус</div>
               <Controller name='status' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
                   <Select className={styles.select} options={options} value={value} onChange={onChange} />
               )}/>
 
+              <div style={{color: '#fff'}}>Фирма клиента</div>
               <Controller name='firm_name' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
                   <input className={styles.input} style={{borderColor: errors.firm_name ? 'red' : 'black'}} placeholder='Фирма клиента' value={value} onChange={onChange}/>
               )}/>
 
+              <div style={{color: '#fff'}}>ФИО перевозчика</div>
               <Controller name='fio_carrier' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
                   <input className={styles.input} style={{borderColor: errors.fio_carrier ? 'red' : 'black'}}  placeholder='ФИО перевозчика' value={value} onChange={onChange}/>
               )}/>
 
+              <div style={{color: '#fff'}}>Телефон перевозчика</div>
               <Controller name='phone_carrier' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
                   <input className={styles.input} style={{borderColor: errors.phone_carrier ? 'red' : 'black'}} placeholder='Телефон перевозчика' value={value} onChange={onChange}/>
               )}/>
 
+              <div style={{color: '#fff'}}>ATI</div>
               <Controller name='ati' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
                   <input className={styles.input} style={{borderColor: errors.ati ? 'red' : 'black'}} placeholder='ATI' value={value} onChange={onChange} type='number'/>
               )}/>
-
+              
+              <div style={{color: '#fff'}}>Комментарии</div>
               <Controller name='comments' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
                   <textarea className={styles.input} style={{borderColor: errors.comments ? 'red' : 'black'}} placeholder='Комментарии' rows={3} value={value} onChange={onChange}/>
               )}/>
 
+              <div style={{color: '#fff'}}>Дата и время</div>
               {errors.created_at && <span style={{color: '#fff'}}>Выберите дату</span>}
               <Controller name='created_at' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
@@ -125,7 +133,7 @@ export default function BidForm() {
               )}/>
               
             </div>
-            <div>
+            <div style={{display: 'flex', alignItems: 'flex-start'}}>
               <Button onClick={handleSubmit((data)=> data.id ? handleUpdate(data) : handleCreate(data))} view="action" type='submit'>{selectedBid ? "Сохранить" : "Создать"}</Button>
             </div>
           </FormProvider>
