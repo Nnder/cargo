3da56399d1c215d9acdac03a702c6de30b21f127
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-25 18:13:00 +0500

    change border color in invalid field

--- src/components/Form/Bid.tsx
+++ src/components/Form/Bid.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useBidStore } from '@/src/zustand/bid';
 import {Button, Modal} from '@gravity-ui/uikit';
-import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 import Select from 'react-select';
 import styles from './Bid.module.css'
@@ -11,6 +10,7 @@ import { useQueryClient } from '@tanstack/react-query';
 import { createBid, updateBid } from '@/src/api/bid';
 import { useSearchStore } from '@/src/zustand/search';
 import { Bid } from '@/src/types/bid.types';
+import "react-datepicker/dist/react-datepicker.css";
 
 const defaultValues = {
   id: "",
@@ -85,7 +85,7 @@ export default function BidForm() {
             <div style={{display: 'flex', flexDirection:'column', justifyContent: 'space-evenly', alignItems:'center', height: '450px',}}>
               <Controller name='id' control={control}
                 render={ ({field: {value, onChange}}) => (
-                  <input className={styles.input}  placeholder='№ заявки' value={value} onChange={onChange} disabled={true}/>
+                  <input className={styles.input} placeholder='№ заявки' value={value} onChange={onChange} disabled={true}/>
               )}/>
 
               <Controller name='status' control={control} rules={{required: true}}
@@ -93,37 +93,32 @@ export default function BidForm() {
                   <Select className={styles.select} options={options} value={value} onChange={onChange} />
               )}/>
 
-              {errors.firm_name && <span style={{color: '#fff'}}>Заполните поле</span>}
               <Controller name='firm_name' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
-                  <input className={styles.input} placeholder='Фирма клиента' value={value} onChange={onChange}/>
+                  <input className={styles.input} style={{borderColor: errors.firm_name ? 'red' : 'black'}} placeholder='Фирма клиента' value={value} onChange={onChange}/>
               )}/>
 
-              {errors.fio_carrier && <span style={{color: '#fff'}}>Заполните поле</span>}
               <Controller name='fio_carrier' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
-                  <input className={styles.input}  placeholder='ФИО перевозчика' value={value} onChange={onChange}/>
+                  <input className={styles.input} style={{borderColor: errors.fio_carrier ? 'red' : 'black'}}  placeholder='ФИО перевозчика' value={value} onChange={onChange}/>
               )}/>
 
-              {errors.phone_carrier && <span style={{color: '#fff'}}>Заполните поле</span>}
               <Controller name='phone_carrier' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
-                  <input className={styles.input} placeholder='Телефон перевозчика' value={value} onChange={onChange}/>
+                  <input className={styles.input} style={{borderColor: errors.phone_carrier ? 'red' : 'black'}} placeholder='Телефон перевозчика' value={value} onChange={onChange}/>
               )}/>
 
-              {errors.ati && <span style={{color: '#fff'}}>Заполните поле</span>}
               <Controller name='ati' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
-                  <input className={styles.input} placeholder='ATI' value={value} onChange={onChange} type='number'/>
+                  <input className={styles.input} style={{borderColor: errors.ati ? 'red' : 'black'}} placeholder='ATI' value={value} onChange={onChange} type='number'/>
               )}/>
 
-              {errors.comments && <span style={{color: '#fff'}}>Заполните поле</span>}
               <Controller name='comments' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
-                  <textarea className={styles.input} placeholder='Комментарии' rows={3} value={value} onChange={onChange}/>
+                  <textarea className={styles.input} style={{borderColor: errors.comments ? 'red' : 'black'}} placeholder='Комментарии' rows={3} value={value} onChange={onChange}/>
               )}/>
 
-              {errors.created_at && <span style={{color: '#fff'}}>Заполните поле</span>}
+              {errors.created_at && <span style={{color: '#fff'}}>Выберите дату</span>}
               <Controller name='created_at' control={control} rules={{required: true}}
                 render={ ({field: {value, onChange}}) => (
                   <div><DatePicker className={styles.datepicker} selected={value} showTimeSelect onChange={onChange} dateFormat="Pp"/></div>
